4a0892d2329beda2f6adfd1c7864c12b00df3fce
CaseStudyShiga <CaseStudyShiga@gmail.com>
2017-04-24 15:30:01 +0900

    ベースクラスの作成

---
+++ CaseStudyProj/Assets/Scenes/Game/Base/CharBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharBase
+{
+	string _name;
+	int _attack;
+	int _hp;
+	int _move;
+
+	public string Name { get { return _name; } set { _name = value; } }
+	public int Attack { get { return _attack; } set { _attack = value; } }
+	public int Hp { get { return _hp; } set { _hp = value; } }
+	public int Move { get { return _move; } set { _move = value; } }
+}
---
+++ CaseStudyProj/Assets/Scenes/Game/Base/SkillBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class SkillBase
+{
+	string _name;
+	string _content;
+	int _type;
+	int _value;
+
+	public string Name { get { return _name; } set { _name = value; } }
+	public string Content { get { return _content; } set { _content = value; } }
+	public int Type { get { return _type; } set { _type = value; } }
+	public int Value { get { return _value; } set { _value = value; } }
+}
+	
