a566c87acd36d54822d32696ba7f0c042cd376a6
CaseStudyShiga <CaseStudyShiga@gmail.com>
2017-06-02 18:48:35 +0900

    石井氏のタイトルを結合

---
+++ CaseStudyProj/Assets/Scenes/Ishii/Titlelogo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Titlelogo : MonoBehaviour {
+    
+    private Image renderer;
+
+    public bool bUse = false;      // 使用されているか
+    private bool bAdd = true;
+    public float a_color = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        renderer = GetComponent<Image>();
+        a_color = 0.0f;
+        renderer.color = new Color(255, 255, 255, a_color);
+        bUse = false;
+        bAdd = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bUse)
+        {
+            renderer.color = new Color(255, 255, 255, a_color);
+
+            // アルファ値増減　true増加　false減少
+            if (bAdd)
+            {
+                a_color += 0.02f;
+                if (a_color >= 1.0f) bAdd = false;
+            }
+            else
+            {
+                a_color -= 0.02f;
+                if (a_color <= 0.0f) bAdd = true;
+            }
+        }
+        else
+        {
+            var bMainChange = this.transform.parent.Find("TestTitle").GetComponent<TitleTouch>().bUse;
+            if (bMainChange)
+            {
+                bUse = true;
+            }
+        }
+    }
+   
+}
